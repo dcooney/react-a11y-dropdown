35759ec722159b8b5e3c950df653796ab6ef8e3e
Darren Cooney <darren.cooney@gmail.com>
2023-09-01 09:16:52 -0400

    Updated docs content

--- src/demo/components/Footer.js
+++ src/demo/components/Footer.js
@@ -1,14 +1,13 @@
 export default function Footer() {
    return (
       <div>
-         <p className="text-sm py-10 leading-5 p-12 text-center m-0">
+         <p className="text-sm py-10 leading-5 p-12 text-center m-0 ita">
             <strong>Note</strong>:{' '}
             <a href="https://www.npmjs.com/package/react-a11y-dropdown">
-               React A11y Dropdowns
+               React A11y Dropdown
             </a>{' '}
-            does not style the contents of dropdowns. It's simply a wrapper for
-            your existing components and will inherit application styling when
-            required.
+            does not style dropdown contents. It's a wrapper for existing
+            components and will inherit application styling.
          </p>
          <footer className="pt-5 pb-20 border-t">
             <ul className="flex justify-center gap-3 text-xs">
